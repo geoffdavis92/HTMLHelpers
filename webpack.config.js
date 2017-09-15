cb955ba5e5438e619e932421b0edd78f592838b8
Geoff Davis <gdavis@archermalmo.com>
2017-09-15 17:33:44 -0500

    config updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,48 @@ const path = require("path");
 const webpack = require("webpack");
 
 const source = function(dir) {
-  return __dirname + "/source/" + dir;
+	return __dirname + "/source/" + dir;
 };
-const dest = function(dir='') {
-  return __dirname + "/dist/" + dir;
+const dest = function(dir = "") {
+	return __dirname + "/dist/" + dir;
 };
 
 const isDev = process.env.NODE_ENV === "development" ? true : false;
 
 module.exports = {
-  entry: source("index.js"),
-  output: {
-    path: dest(),
-    filename: "index.js?hash=[hash]"
-  },
-  watch: false,
-  resolve: {
-    alias: {
-      classes: path.resolve(__dirname, "source/classes"),
-      functions: path.resolve(__dirname, "source/functions"),
-      helpers: path.resolve(__dirname, "source/_helpers"),
-      types: path.resolve(__dirname, "source/types"),
-      tests: path.resolve(__dirname, "source/tests"),
-      utilities: path.resolve(__dirname, "source/utilities"),
-    }
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  },
-  plugins: isDev
-    ? []
-    : [
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            warnings: false,
-            drop_console: false
-          }
-        })
-      ]
+	entry: source("index.js"),
+	output: {
+		path: dest(),
+		filename: "index.js?hash=[hash]"
+	},
+	watch: false,
+	resolve: {
+		alias: {
+			classes: path.resolve(__dirname, "source/classes"),
+			functions: path.resolve(__dirname, "source/functions"),
+			helpers: path.resolve(__dirname, "source/_helpers"),
+			types: path.resolve(__dirname, "source/types"),
+			tests: path.resolve(__dirname, "source/tests"),
+			utilities: path.resolve(__dirname, "source/utilities")
+		}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	plugins: isDev
+		? []
+		: [
+				new webpack.optimize.UglifyJsPlugin({
+					compress: {
+						warnings: false,
+						drop_console: false
+					}
+				})
+			]
 };
